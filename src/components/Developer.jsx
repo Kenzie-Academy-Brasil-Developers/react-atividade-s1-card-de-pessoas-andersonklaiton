fd3bb50966c82e2bbca77fa9d4aa7306bacc2560
AndersonS <klaito.b@gmail.com>
2021-08-31 15:10:07 -0300

    Initial commit

---
+++ src/components/Developer.jsx
@@ -0,0 +1,14 @@
+import "./style.css"
+
+function Developer({name, age, country}){
+    return(
+        <div className="cardInfo">
+            <span>
+                <p className="userName">Dev: {name}</p>
+                <p>Idade: {age}</p>
+                <p>País: {country}</p>
+            </span>
+        </div>
+    )
+}
+export default Developer
