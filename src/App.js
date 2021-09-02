ef916a5a3de204ece84d8fb9914c672e12828904
AndersonS <klaito.b@gmail.com>
2021-09-01 22:39:54 -0300

    modified array of objects

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import './App.css';
 import Developer from './components/Developer'
 
 function App() {
-  const userGabriel = ["Gabriel", 19, "Brasil"]
-  const userFilipe = ["Filipe", 28, "Brasil"]
-  const userMariana = ["Mariana", 24, "Itália"]
+
   return (
     <div className="App">
       <div className="App-header">
-        <Developer name={userGabriel[0]} age={userGabriel[1]} country={userGabriel[2]}/>
-        <Developer name={userFilipe[0]} age={userFilipe[1]} country={userFilipe[2]}/>
-        <Developer name={userMariana[0]} age={userMariana[1]} country={userMariana[2]}/>
+        <Developer></Developer>
       </div>
     </div>
   );
--- src/components/Developer.jsx
+++ src/components/Developer.jsx
@@ -1,14 +1,32 @@
 import "./style.css"
 
-function Developer({name, age, country}){
-    return(
-        <div className="cardInfo">
-            <span>
-                <p className="userName">Dev: {name}</p>
-                <p>Idade: {age}</p>
-                <p>País: {country}</p>
-            </span>
-        </div>
-    )
+function Developer(){
+    const person=[
+        {name:"Gabriel",
+         age:19,
+         country:"Brasil"
+        },
+        {name:"Filipe",
+         age:28,
+         country:"Brasil"
+        },
+        {name:"Mariana",
+         age:24,
+         country:"Itália"
+        },
+    ]
+
+    const eachPerson = person.map((item)=>{
+        return (
+            <div className="cardInfo">
+                <span>
+                    <p className="userName">Dev: {item.name}</p>
+                    <p>Idade: {item.age}</p>
+                    <p>País: {item.country}</p>
+                </span>
+            </div>
+        )
+    })
+    return eachPerson
 }
 export default Developer
